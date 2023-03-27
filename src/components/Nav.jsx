bc98622a67a4c7415f5de29addcd64a4ce6c1b44
Wan Jong Kim <wkim2@9152.local>
2023-03-27 15:28:37 -0400

    Working on Portfolio section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Nav = () => {
 
+    const showMenu = () => {
+        
+    }
+
     return (
-        <div className="fixed w-screen bg-slate-800 text-slate-300">
+        <div className="z-10 fixed w-screen bg-slate-800 text-slate-300">
             <nav className="flex justify-between px-6 py-4 items-center">
                 <div className="px-1 py-1">
                     <span className="headers_font text-2xl font-bold">Wan Jong Kim</span>
@@ -15,7 +19,7 @@ const Nav = () => {
                         <li className="headers_font text-xl">Contact</li>
                     </ul>
                 </div>
-                <div className="sm:hidden rounded-md px-1 py-1 bg-red-600 text-white">
+                <div className="sm:hidden rounded-md px-1 py-1 bg-red-600 text-white cursor-pointer" onClick={showMenu()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styled from "styled-components"
 
-const Section = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
-`
 
 const Works = () => {
     return (
-        <Section>
-            Works
-        </Section>
+        <div className="flex flex-col mt-16 px-6">
+            <div className="mb-5">
+                <h2 className="text-[#34a8eb] text-xl font-bold mb-2">PORTFOLIO</h2>
+                <p className="text-lg font-bold">Each project is a unique piece of development</p>
+            </div>
+            <div className="flex flex-col">
+                <img className="rounded-2xl" src="https://cdn.cms-twdigitalassets.com/content/dam/blog-twitter/official/en_us/products/2022/recommendations-on-twitter/how-recommendations-help-discover-more-twitter-1.jpg.twimg.768.jpg" />
+                <div className="flex flex-col text-center">
+                    <h2 className="my-4 text-2xl">Sociopedia</h2>
+                    <p className="text-lg">Sociopedia is an online social media site and its primary purpose is to connect people and allow people to share their thoughts with a big audience.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
